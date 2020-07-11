2341dfbd5a8222c1d32c5f010ae4bc612b702f0e
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-10 20:16:29 -0700

    render an image via graphql useStaticQuery

--- src/components/ImageHero/ImageHero.js
+++ src/components/ImageHero/ImageHero.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import './ImageHero.css';
 
 export default () => {
-  return (<div className="my-class">
-    Hi
-  </div>)
-}
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "andreas-haslinger-dandelion.jpg" }) {
+        childImageSharp {
+          fluid {
+            base64
+            aspectRatio
+            sizes
+            src
+            srcSet
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <Fragment>
+      <Img fluid={data.file.childImageSharp.fluid} alt="A peaceful dandelion" />
+    </Fragment>
+  );
+};
