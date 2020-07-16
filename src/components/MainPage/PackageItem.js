ada956f56bd396e8fbacaffbd1c18dc148479a6d
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-15 22:47:49 -0700

    add text image for background, enlargen text of cards

--- src/components/MainPage/PackageItem.js
+++ src/components/MainPage/PackageItem.js
@@ -3,7 +3,8 @@ import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 import './MainPage.css';
 
-const PackageItem = ({ itemDetails: { title, description, price, strapiId, picture }, image }) => {
+const PackageItem = ({ itemDetails: { title, description, price, strapiId, picture }, patternImage }) => {
+  console.log(patternImage)
   return (
     <article className="package-item__card">
       <Img
@@ -17,7 +18,7 @@ const PackageItem = ({ itemDetails: { title, description, price, strapiId, pictu
           borderRadius: '8px 8px 0 0'
         }}
       />
-      <h5 className="package-item__card-title">{title}</h5>
+      <h5 className="package-item__card-title" style={{ backgroundImage: `url(${patternImage})`, backgroundSize: '15%' }}>{title}</h5>
       <p className="package-item__card-description">{description}</p>
       <Link to={`/package/${strapiId}`}><button className="package-item__button">Find out more...</button></Link>
     </article>
--- src/components/MainPage/PackageItemList.js
+++ src/components/MainPage/PackageItemList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import PackageItem from './PackageItem';
-import ReactMarkdown from 'react-markdown';
+// import ReactMarkdown from 'react-markdown';
 import './MainPage.css';
 
 export default () => {
   const data = useStaticQuery(graphql`
     query {
+      hexagon: file(relativePath: { eq: "hexagonalpattern.png" }) {
+        childImageSharp {
+          fluid(fit: CONTAIN) {
+            originalImg
+          }
+        }
+      }
       allStrapiPackageItem {
         nodes {
           price
@@ -30,18 +37,19 @@ export default () => {
       }
     }
   `);
-  
+
   return (
     <section className="main-page__section">
       <h3 className="cinzel main-page__title">Services</h3>
       <div className="main-page__package-item-list">
         {data.allStrapiPackageItem.nodes
           .sort((a, b) => a.strapiId - b.strapiId)
-          .map((item) => (
+          .map(item => (
             <PackageItem
               key={item.id}
               itemDetails={item}
               strapiId={item.strapiId}
+              patternImage={data.hexagon.childImageSharp.fluid.originalImg}
             />
           ))}
       </div>
