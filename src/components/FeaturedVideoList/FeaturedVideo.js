15731c59d1ef32fa65bc032d66b16a6107e86053
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-17 14:16:10 -0700

    test async loading of video iframe to improve performance metrics

--- src/components/FeaturedVideoList/FeaturedVideo.js
+++ src/components/FeaturedVideoList/FeaturedVideo.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FeaturedVideoList.css';
 
 export default ({ videoSrcUrl, videoTitle }) => {
+  const [showVideo, setShowVideo] = useState(false);
+  // video is loaded asyncronously to improve performance metrics
+  useEffect(() => {
+    const onLoad = () => {
+      setShowVideo(true);
+    };
+    window.addEventListener('load', onLoad());
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
   return (
     <div className="video-container">
-      <iframe
-        className="video-iframe"
-        src={videoSrcUrl}
-        title={videoTitle}
-        allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
-        frameBorder="0"
-        webkitallowfullscreen="true"
-        mozallowfullscreen="true"
-        allowFullScreen
-      />
+      {showVideo && (
+        <iframe
+          className="video-iframe"
+          src={videoSrcUrl}
+          title={videoTitle}
+          allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowFullScreen
+        />
+      )}
     </div>
   );
 };
