0b7f2626820925245ba1fb826ad0985e75dfa94f
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-18 00:17:27 -0700

    addforgotten hyphen

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,7 +40,7 @@ const Header = ({ siteTitle }) => {
       </nav>
       <button
         className="burger-menu"
-        arialabel="Toggle Navigation Menu"
+        aria-label="Toggle Navigation Menu"
         type="button"
         onClick={toggleFlyout}
       >
