be5e948f141f05506ca109153d208c9957d98da2
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-24 16:40:39 -0700

    change logic for dynamic url pulling

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.API_URL || 'http://localhost:1337';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : 'https://www.dharmicastrology.herokuapp.com';
